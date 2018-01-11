2021747e7b5ce24b7d1c19e3415d2fb6f35111bf
Anthony Acosta <30398741+anedacos@users.noreply.github.com>
2018-01-11 14:31:57 -0500

    Quinto ejemplo

---
+++ Dia_SwitchStatements_Mal.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package diseñodesoftware;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Dia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int semana =1;
+       if(semana==0) System.out.println("Domingo");
+       if(semana==1) System.out.println("Lunes");
+       if(semana==2) System.out.println("Martes");
+       if(semana==3) System.out.println("Miercoles");
+       if(semana==4) System.out.println("Jueves");
+       if(semana==5) System.out.println("Viernes");
+       if(semana==6) System.out.println("Sabado");
+    }
+    
+}
