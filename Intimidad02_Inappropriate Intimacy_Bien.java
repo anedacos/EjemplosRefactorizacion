598e1a12fd258ecd818d55c1f881ddb5c54a4323
Anthony Acosta <anedacos@gmail.com>
2018-01-11 21:17:50 -0500

    Doceavo ejemplo

---
+++ Intimidad02_Inappropriate Intimacy_Bien.java
@@ -0,0 +1,34 @@
+package tareadiseño;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Antho
+ */
+public class Intimidad02 {
+    String nombre, codigo;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    
+    public Intimidad02(){
+        
+    }
+    
+    public String descripcion(){
+        Intimidad01 i = new Intimidad01();
+        return nombre+" "+codigo;
+    }
+    
+}
